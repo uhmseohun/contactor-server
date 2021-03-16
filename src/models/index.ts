996c4defe69de9363bf1ec09141e844d100fc591
Seohun Uhm <uhmtotow@gmail.com>
2021-03-16 09:46:21 +0900

    feat: add location log model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 import 'ts-mongoose/plugin';
 
 export * from './user';
+export * from './location-log';
---
+++ src/models/location-log.ts
@@ -0,0 +1,14 @@
+import { createSchema, Type, typedModel } from 'ts-mongoose';
+import { userSchema } from './user';
+
+const locationLogSchema = createSchema({
+  location: Type.object({ required: true }).of({
+    lat: Type.number({ required: true }),
+    lan: Type.number({ required: true }),
+  }),
+  user: Type.ref(Type.objectId({ required: true })).to('User', userSchema),
+}, { versionKey: false, timestamps: true });
+
+const LocationLogModel = typedModel('LocationLog', locationLogSchema);
+
+export { locationLogSchema, LocationLogModel };
