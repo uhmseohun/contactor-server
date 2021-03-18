9041ccf632f8bf2b9ffcf2386d3f5c79832289fa
Seohun Uhm <uhmtotow@gmail.com>
2021-03-18 16:17:05 +0900

    Body 데이터 대신 URL 파라미터로 좌표 전달

--- src/services/vaccine-center/controllers.ts
+++ src/services/vaccine-center/controllers.ts
@@ -5,13 +5,12 @@ import { getDist } from '../../resources/coordinate';
 import centers from './data.json';
 
 export const getNearbyVaccineCenters = (req: Request, res: Response) => {
-  if (!req.body.lat || !req.body.lng) throw new HttpException(
+  if (!req.query.lat || !req.query.lng) throw new HttpException(
     400, '좌표 값이 전달되지 않았습니다.',
     );
-    console.log(req.body)
   const coord = {
-    lat: parseFloat(req.body.lat as string),
-    lng: parseFloat(req.body.lng as string),
+    lat: parseFloat(req.query.lat as string),
+    lng: parseFloat(req.query.lng as string),
   };
   const sortedCenters = centers.sort((a: VaccineCenter, b: VaccineCenter) => (
     getDist(a, coord) - getDist(b, coord)
